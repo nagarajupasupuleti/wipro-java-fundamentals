3d320836fd8b77fb0447f4c8a76814f0ec5410a7
nagarajupasupuleti <pasupuletinagaraju681@gmail.com>
2019-06-24 10:05:16 +0530

    Wipro java fundamentals
    Wipro java fundamentals

---
+++ Pro_2.java
@@ -0,0 +1,9 @@
+
+public class Pro_2 {
+	
+	public static void main(String args[]){
+		
+		System.out.println(args[0]+" Technologis "+args[1]);
+	}
+
+}
---
+++ Pro_3.java
@@ -0,0 +1,10 @@
+public class Pro_3 {
+	public static void main(String args[]){
+		
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		
+		System.out.println("the sum of"+a+" and "+b+" is "+(a+b));
+	}
+
+}
